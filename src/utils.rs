6366192a4acea7e03dfaeca6ffe655413249b2ff
rainwolf <rainwolf@submanifold.be>
2026-02-01 22:01:03 +0100

    ch2 ex4a + plotly plot

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,8 @@
 use polars::prelude::*;
+use build_html::*;
+use std::env;
+use std::{fs::File, io::Write, process::Command};
+use plotly::Trace;
 
 pub fn load_data() -> LazyFrame {
     let data_set = LazyFrame::scan_parquet(
@@ -9,4 +13,34 @@ pub fn load_data() -> LazyFrame {
     // let print_set = data_set.clone();
     // println!("{:?}", print_set.first().collect().unwrap());
     data_set
+}
+
+pub fn show_plot(traces: Vec<Box<dyn Trace>>) {
+    let mut base = HtmlPage::new()
+        .with_title("Plotly-rs Multiple Plots")
+        .with_script_link("https://cdn.plot.ly/plotly-latest.min.js")
+        .with_header(1, "Multiple Plotly plots on the same HTML page");
+    use plotly::Plot;
+    for (i, trace) in traces.iter().enumerate() {
+        let mut plot = Plot::new();
+        plot.add_trace(trace.clone());
+        base.add_raw(plot.to_inline_html(Some(format!("test_{}", i).as_str())).as_str());
+    }
+    let html = base.to_html_string();
+
+    let mut temp = env::temp_dir();
+    temp.push("plotly.html");
+
+    // Save the rendered plot to the temp file.
+    let temp_path = temp.to_str().unwrap();
+    {
+        let mut file = File::create(temp_path).unwrap();
+        file.write_all(html.as_bytes())
+            .expect("failed to write html output");
+        file.flush().unwrap();
+    }
+    Command::new("open")
+        .args([temp_path])
+        .output()
+        .expect("DEFAULT_HTML_APP_NOT_FOUND");
 }
